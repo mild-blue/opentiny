64d41d2a50f74dc6b9c7804e52e93c58a41562c2
Miriam Zednickova <mitcamiit@gmail.com>
2025-02-19 17:23:22 +0100

    keep isPagebreak in the isImage function

--- modules/tinymce/src/plugins/quickbars/main/ts/selection/Toolbars.ts
+++ modules/tinymce/src/plugins/quickbars/main/ts/selection/Toolbars.ts
@@ -11,13 +11,13 @@ const addToEditor = (editor: Editor): void => {
   const isImage = (node: Element): boolean => {
     const isImageFigure = node.nodeName === 'FIGURE' && /image/i.test(node.className);
     const isImage = node.nodeName === 'IMG' || isImageFigure;
-    return isImage && isInEditableContext(node);
+    return isImage && isInEditableContext(node) && !isPagebreak(node);
   };
 
   const imageToolbarItems = Options.getImageToolbarItems(editor);
   if (imageToolbarItems.length > 0) {
     editor.ui.registry.addContextToolbar('imageselection', {
-      predicate: (node) => isImage(node) && !isPagebreak(node),
+      predicate: (node) => isImage(node),
       items: imageToolbarItems,
       position: 'node'
     });
@@ -26,7 +26,7 @@ const addToEditor = (editor: Editor): void => {
   const textToolbarItems = Options.getTextSelectionToolbarItems(editor);
   if (textToolbarItems.length > 0) {
     editor.ui.registry.addContextToolbar('textselection', {
-      predicate: (node) => !isImage(node) && !editor.selection.isCollapsed() && isEditable(node) && !isPagebreak(node),
+      predicate: (node) => !isImage(node) && !isPagebreak(node) && !editor.selection.isCollapsed() && isEditable(node) && !isPagebreak(node),
       items: textToolbarItems,
       position: 'selection',
       scope: 'editor'
