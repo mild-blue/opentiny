0816cc4f416c6b0b77eb5111666b7ff7485397cb
Miriam Zedníčková <113028848+chirimiri22@users.noreply.github.com>
2025-02-19 18:05:13 +0100

    Dont show any quick toolbar for page break (#27)
    * dont show any quick toolbar for page break
    
    * keep isPagebreak in the isImage function
    
    ---------
    
    Co-authored-by: Miriam Zednickova <mitcamiit@gmail.com>

--- modules/tinymce/src/plugins/quickbars/main/ts/selection/Toolbars.ts
+++ modules/tinymce/src/plugins/quickbars/main/ts/selection/Toolbars.ts
@@ -1,4 +1,4 @@
-import { Class, SugarElement } from '@ephox/sugar';
+import {Class, SugarElement} from '@ephox/sugar';
 
 import Editor from 'tinymce/core/api/Editor';
 
@@ -7,17 +7,17 @@ import * as Options from '../api/Options';
 const addToEditor = (editor: Editor): void => {
   const isEditable = (node: Element | null): boolean => editor.dom.isEditable(node);
   const isInEditableContext = (el: Element) => isEditable(el.parentElement);
+  const isPagebreak = (node: Element) => Class.has(SugarElement.fromDom(node), 'mce-pagebreak');
   const isImage = (node: Element): boolean => {
     const isImageFigure = node.nodeName === 'FIGURE' && /image/i.test(node.className);
     const isImage = node.nodeName === 'IMG' || isImageFigure;
-    const isPagebreak = Class.has(SugarElement.fromDom(node), 'mce-pagebreak');
-    return isImage && isInEditableContext(node) && !isPagebreak;
+    return isImage && isInEditableContext(node) && !isPagebreak(node);
   };
 
   const imageToolbarItems = Options.getImageToolbarItems(editor);
   if (imageToolbarItems.length > 0) {
     editor.ui.registry.addContextToolbar('imageselection', {
-      predicate: isImage,
+      predicate: (node) => isImage(node),
       items: imageToolbarItems,
       position: 'node'
     });
@@ -26,7 +26,7 @@ const addToEditor = (editor: Editor): void => {
   const textToolbarItems = Options.getTextSelectionToolbarItems(editor);
   if (textToolbarItems.length > 0) {
     editor.ui.registry.addContextToolbar('textselection', {
-      predicate: (node) => !isImage(node) && !editor.selection.isCollapsed() && isEditable(node),
+      predicate: (node) => !isImage(node) && !isPagebreak(node) && !editor.selection.isCollapsed() && isEditable(node) && !isPagebreak(node),
       items: textToolbarItems,
       position: 'selection',
       scope: 'editor'
