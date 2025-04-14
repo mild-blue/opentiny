2754dd8424abbe7d3fbb8d0705441316b30565c0
curdopet <74628055+curdopet@users.noreply.github.com>
2025-04-14 10:07:43 +0200

    Update modules/tinymce/src/core/main/ts/delete/BlockRangeDelete.ts

--- modules/tinymce/src/core/main/ts/delete/BlockRangeDelete.ts
+++ modules/tinymce/src/core/main/ts/delete/BlockRangeDelete.ts
@@ -87,7 +87,7 @@ const deleteSymbol = (editor: Editor): Optional<() => void> => {
     return Optional.none();
   }
   const editableDiv = getClosestEditableDiv(editor);
-  if(editableDiv?.textContent?.length == 1 || editableDiv?.textContent?.length == 0){
+  if(editableDiv?.textContent?.length <= 1){
     return emptyEditableDiv(editor, editableDiv);
   }
   return Optional.none();
